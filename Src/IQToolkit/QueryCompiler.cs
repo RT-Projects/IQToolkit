18d605e2928fc3bb9af2fd4d1d9ef72c1b188066
rstarkov <sourcecode@roman.st>
2010-05-28 22:32:46 +0100

    * [IQToolkit]: added an alternative implementation of the compiled queries: CompiledQuery + generic descendants. * [SqlChainTests]: use new compiled queries; minor changes; test connection being closed and reopened. Disabled failing bits. (imported from Perforce changelist 2047)

--- Src/IQToolkit/QueryCompiler.cs
+++ Src/IQToolkit/QueryCompiler.cs
@@ -241,4 +241,158 @@ namespace IQToolkit
             }
         }
     }
+
+    /// <summary>Base class for compiled queries. See Remarks for various conventions and notes.</summary>
+    /// <remarks>
+    /// <para>The compiled query must either capture a local that evaluates to the IQueryable being queried, or supply an IQueryProvider upon compilation.
+    /// In the former scenario, the compiled query will only work on the given connection. In the latter scenario, to support a separate connection being
+    /// used the IQueryable operated on should be one of the arguments taken by the query. Note that violating the former requirement may actually
+    /// work on some providers, e.g. sqlite, but this should not be relied upon.</para>
+    /// <para>This class and descendants are an alternative implementation to the one in <see cref="QueryCompiler"/>.</para>
+    /// </remarks>
+    public abstract class CompiledQuery
+    {
+        public static CompiledQuery<TResult> Create<TResult>(Expression<Func<TResult>> query) { return new CompiledQuery<TResult>(query); }
+        public static CompiledQuery<T, TResult> Create<T, TResult>(Expression<Func<T, TResult>> query) { return new CompiledQuery<T, TResult>(query); }
+
+        /// <summary>Compiles the <see cref="_Query"/> and places the result into <see cref="_Compiled"/>, unless there already is a result in <see cref="_Compiled"/>.</summary>
+        /// <param name="args">If not null, the arguments will be searched for a query provider in case the expression doesn't have one as a ConstantExpression.</param>
+        protected static Delegate Compile(LambdaExpression query, IQueryProvider provider, object[] args)
+        {
+            // attempt to locate a provider
+            if (provider == null)
+                provider = findProvider(query.Body, args, query.Parameters.ToArray());
+            if (provider == null)
+                throw new InvalidOperationException("Could not find query provider");
+
+            // ask the query provider to compile the query by 'executing' the lambda expression
+            return (Delegate) provider.Execute(query);
+        }
+
+        private static IQueryProvider findProvider(Expression expression, object[] args, ParameterExpression[] @params)
+        {
+            Expression root = findProviderInExpression(expression);
+            if (!(root is ConstantExpression) && args != null && args.Length > 0)
+            {
+                Expression replaced = ExpressionReplacer.ReplaceAll(
+                    expression,
+                    @params,
+                    args.Select((a, i) => Expression.Constant(a, @params[i].Type)).ToArray()
+                    );
+                root = findProviderInExpression(replaced);
+            }
+            if (root != null)
+            {
+                ConstantExpression cex = root as ConstantExpression;
+                if (cex == null)
+                {
+                    cex = PartialEvaluator.Eval(root) as ConstantExpression;
+                }
+                if (cex != null)
+                {
+                    IQueryProvider provider = cex.Value as IQueryProvider;
+                    if (provider == null)
+                    {
+                        IQueryable query = cex.Value as IQueryable;
+                        if (query != null)
+                        {
+                            provider = query.Provider;
+                        }
+                    }
+                    return provider;
+                }
+            }
+            return null;
+        }
+
+        private static Expression findProviderInExpression(Expression expression)
+        {
+            Expression root = TypedSubtreeFinder.Find(expression, typeof(IQueryProvider));
+            if (root == null)
+            {
+                root = TypedSubtreeFinder.Find(expression, typeof(IQueryable));
+            }
+            return root;
+        }
+    }
+
+    /// <summary>Encapsulates a compiled query taking no arguments and returning a result.</summary>
+    /// <typeparam name="TResult">Type of the query result.</typeparam>
+    public sealed class CompiledQuery<TResult> : CompiledQuery
+    {
+        private Expression<Func<TResult>> _query;
+        private Func<TResult> _compiled;
+
+        /// <summary>Constructor.</summary>
+        /// <param name="query">The query to be compiled, as an expression.</param>
+        public CompiledQuery(Expression<Func<TResult>> query)
+        {
+            if (query == null) throw new ArgumentNullException();
+            _query = query;
+        }
+
+        private void compile(IQueryProvider provider, object[] args)
+        {
+            var compiled = CompiledQuery.Compile(_query, provider, args);
+            System.Threading.Interlocked.CompareExchange(ref _compiled, (Func<TResult>) compiled, null);
+        }
+
+        /// <summary>Compiles the query (unless it's already been compiled earlier). Returns a delegate that represents the compiled query.</summary>
+        /// <param name="provider">In cases where the query expression itself doesn't reference a queryable *instance*, a query provider must be supplied during compilation.</param>
+        public Func<TResult> Compile(IQueryProvider provider = null)
+        {
+            if (_compiled == null)
+                compile(provider, null);
+            return _compiled;
+        }
+
+        /// <summary>Executes the compiled query. The query will be compiled at this point if this hasn't been done yet.</summary>
+        public TResult Invoke()
+        {
+            if (_compiled == null)
+                compile(null, null);
+            return _compiled();
+        }
+    }
+
+    /// <summary>Encapsulates a compiled query taking no arguments and returning a result.</summary>
+    /// <typeparam name="T">Type of the argument taken by the query.</typeparam>
+    /// <typeparam name="TResult">Type of the query result.</typeparam>
+    public sealed class CompiledQuery<T, TResult> : CompiledQuery
+    {
+        private Expression<Func<T, TResult>> _query;
+        private Func<T, TResult> _compiled;
+
+        /// <summary>Constructor.</summary>
+        /// <param name="query">The query to be compiled, as an expression.</param>
+        public CompiledQuery(Expression<Func<T, TResult>> query)
+        {
+            if (query == null) throw new ArgumentNullException();
+            _query = query;
+        }
+
+        private void compile(IQueryProvider provider, object[] args)
+        {
+            var compiled = CompiledQuery.Compile(_query, provider, args);
+            System.Threading.Interlocked.CompareExchange(ref _compiled, (Func<T, TResult>) compiled, null);
+        }
+
+        /// <summary>Compiles the query (unless it's already been compiled earlier). Returns a delegate that represents the compiled query.</summary>
+        /// <param name="provider">In cases where the query expression itself doesn't reference a queryable *instance*, a query provider must be supplied during compilation.</param>
+        public Func<T, TResult> Compile(IQueryProvider provider = null)
+        {
+            if (_compiled == null)
+                compile(provider, null);
+            return _compiled;
+        }
+
+        /// <summary>Executes the compiled query. The query will be compiled at this point if this hasn't been done yet.</summary>
+        public TResult Invoke(T arg)
+        {
+            if (_compiled == null)
+                compile(null, null);
+            return _compiled(arg);
+        }
+    }
+
 }
